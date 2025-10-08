1684cd84f5e023d4d0b12e0737ea87232f6220f2
Handock-max <martindetours98@gmail.com>
2025-10-08 17:31:39 +0000

    Change to normal

---
+++ env.js
@@ -0,0 +1,6 @@
+// Configuration environment
+window.env = {
+  // Remplacez cette URL par votre véritable webhook Slack
+  SLACK_WEBHOOK_URL: 'https://hooks.slack.com/services/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'
+};
+// Configuration environment
